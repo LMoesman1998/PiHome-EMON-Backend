15edf9a1cd98c3e81eeb3ae0ae0fc90646c783d8
Lars Moesman <lars.moesman@gmail.com>
2020-09-09 17:12:41 +0200

    [ADDED] SMQTT

--- EMON-Handler/src/index.js
+++ EMON-Handler/src/index.js
@@ -1,12 +1,27 @@
 import mqtt from 'mqtt';
 import moment from 'moment';
 
+import * as fs from 'fs';
 import { brokerUrl, brokerOptions, meterTopic} from './../../config';
 import { connectMongo } from './db';
 import { processTelegram } from './emon-handler';
 
-const client = mqtt.connect(brokerUrl, brokerOptions);
-let proceedMQTT = false;
+const KEY = fs.readFileSync('../certs/client.key')
+const CERT = fs.readFileSync('../certs/client.crt')
+const TRUSTED_CA_LIST = fs.readFileSync('../certs/ca.crt')
+
+const tlsOptions = {
+    key: KEY,
+    cert: CERT,
+    ca: TRUSTED_CA_LIST,
+    protocol: 'mqtts'
+  };
+  
+  console.log("start")
+  
+  const client = mqtt.connect(brokerUrl, { ...brokerOptions, ...tlsOptions });
+ 
+let proceedMQTT = true;
 
 const init = async () => {
     await connectMongo()
@@ -28,12 +43,13 @@ client.on('connect', () => {
 });
 
 client.on('message', async (topic, message) => {
+    
     if (!proceedMQTT) return;
     switch (topic) {
         case meterTopic: {
             const jsonMessage = JSON.parse(message.toString());
             const processedData = await processTelegram(jsonMessage.telegram);
-           // console.log(processedData);
+            //console.log(processedData);
             // Handle message
             // Publish on topic
             break;
